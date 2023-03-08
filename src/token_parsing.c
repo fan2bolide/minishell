ba6a3e853ce63bd31cb52237e733d05228778e15
basil <basiljeannot@icloud.com>
2023-03-08 13:47:23 +0100

    updated checks for parsing analysis

--- src/token_parsing.c
+++ src/token_parsing.c
@@ -12,6 +12,11 @@
 
 #include "lexer.h"
 
+static int	check_all_consecutives_types(t_list *list);
+static int	are_consecutives_types(t_list *list, t_type type1, t_type type2);
+static int	types_following_each_other(t_list *list, t_type type1, \
+t_type type2);
+
 /*
  * checks for parsing rules
  * RULE 1 : there can't be two consecutive redirect operator
@@ -19,9 +24,6 @@
  * redirect operator)
  * RULE 3 : There can't be two consecutive pipes (we do not handle minishell
  * bonuses..)
- * RULE 4 : an exec MUST NOT start with a hyphen '-'
- * RULE 5 : an arg MUST start with a hyphen '-'
- * RULE 6 : there can't be two exec_name in the same command
  * RULE 7 : after a redirect comes automatically a file
  * RULE 8 : after a file comes either :
  * 			- an exec_name (ex : "< file cat " )
@@ -36,29 +38,66 @@ t_list	*token_parsing(t_list *tokens)
 	t_token *curr_token;
 
 	curr = tokens;
+	if (!check_all_consecutives_types(tokens))
+		return (NULL);
 	while (curr)
 	{
 		curr_token = curr->content;
 		if (curr_token->type == error)
-			return (ft_lstclear(&tokens, free), NULL);
-		if (curr_token->type != exec_name && curr_token->type != arg &&\
+			return (ft_lstclear(&tokens, destroy_token), NULL);
+		if (curr_token->type != exec_name && curr_token->type != arg && \
 			curr_token->type != file)
 		{
 			curr_token = curr->next->content;
-			if (curr_token->type != exec_name && curr_token->type != arg &&\
+			if (curr_token->type != exec_name && curr_token->type != arg && \
 			curr_token->type != file)
-				return (ft_lstclear(&tokens, free), NULL);
+				return (ft_lstclear(&tokens, destroy_token), NULL);
 		}
 	}
 	return (tokens);
 }
 
-static int	check_consecutives_types(t_list *list, t_type type1, t_type type2)
+int	are_consecutives_types(t_list *list, t_type type1, t_type type2)
 {
 	while (list && list->next)
 	{
 		if (((t_token *)list->content)->type == type1 && \
 		((t_token *)list->next->content)->type == type2)
+			return (0);
+		list = list->next;
+	}
+	return (1);
+}
+
+int check_all_consecutives_types(t_list *tokens)
+{
+	if (!types_following_each_other(tokens, operator_pipe, operator_pipe) || \
+		!types_following_each_other(tokens, redirect_append, operator_pipe) || \
+		!types_following_each_other(tokens, redirect_in, operator_pipe) || \
+		!types_following_each_other(tokens, redirect_out, operator_pipe) || \
+		!types_following_each_other(tokens, redirect_hd, operator_pipe) || \
+		!are_consecutives_types(tokens, redirect_hd, redirect_in) || \
+		!are_consecutives_types(tokens, redirect_hd, redirect_hd) || \
+		!are_consecutives_types(tokens, redirect_hd, redirect_append) || \
+		!are_consecutives_types(tokens, redirect_hd, redirect_out) || \
+		!are_consecutives_types(tokens, redirect_out, redirect_append) || \
+		!are_consecutives_types(tokens, redirect_in, redirect_append) || \
+		!are_consecutives_types(tokens, redirect_append, redirect_append) || \
+		!are_consecutives_types(tokens, redirect_out, redirect_out) || \
+		!are_consecutives_types(tokens, redirect_out, redirect_in) || \
+		!are_consecutives_types(tokens, redirect_in, redirect_in))
+		return (1);
+	return (0);
+}
+
+int	types_following_each_other(t_list *list, t_type type1, t_type type2)
+{
+	while (list && list->next)
+	{
+		if ((((t_token *)list->content)->type == type1 && \
+		((t_token *)list->next->content)->type == type2) || \
+		(((t_token *)list->content)->type == type1 && \
+		((t_token *)list->content)->type == type2))
 			return (0);
 		list = list->next;
 	}
