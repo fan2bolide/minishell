44828eb80b61d3cd1f00ae5e42ed8c1a5f56e635
aLeuleu <97832618+aLeuleu@users.noreply.github.com>
2023-03-09 21:26:44 +0100

    now handles simple commands  + 1 out redirection

--- src/execute_cmd.c
+++ src/execute_cmd.c
@@ -15,7 +15,8 @@
 //'to_read' linked to child ´stdin´ and child ´stdout´ linked to ´to_write´
 void	execute_cmd(t_cmd cmd, int to_read, int to_write)
 {
-	execve(cmd.path, cmd.argv, cmd.envp); //debug
+	dup2(to_read, STDIN_FILENO);
+	dup2(to_write, STDOUT_FILENO);
 	if (cmd.redirect_in)
 	{
 		close(to_read);
@@ -26,10 +27,7 @@ void	execute_cmd(t_cmd cmd, int to_read, int to_write)
 		close(to_write);
 		to_write = open_and_get_fd(cmd.redirect_out, O_WRONLY | cmd.redirect_out_mode | O_CREAT, 0644);
 	}
-	dup2(to_read, STDIN_FILENO);
-	dup2(to_write, STDOUT_FILENO);
-	close(to_read);
-	close(to_write);
+
 	if (execve(cmd.path, cmd.argv, cmd.envp) == -1)
 	{
 		perror("execve");
