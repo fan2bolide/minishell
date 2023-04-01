e7f021bcae5316d66fab9d5fa47dbac1f7b564ab
aLeuleu <97832618+aLeuleu@users.noreply.github.com>
2023-04-01 20:11:02 +0200

    handle absolute and relative paths, does not get stuck if dir removed

--- src/builtins_cd_pwd.c
+++ src/builtins_cd_pwd.c
@@ -4,7 +4,7 @@
 
 #include "builtins.h"
 
-static void update_pwd(const char *dir);
+static void update_pwd();
 
 void pwd(int fd_to_write)
 {
@@ -12,47 +12,13 @@ void pwd(int fd_to_write)
 
 	cwd = ft_calloc(1024 , sizeof(char));
 	if (!cwd)
-		return (ft_putstr_fd("an error occurred (pwd)\nexiting..\n", 2));
+		return (ft_putstr_fd("an error occurred (pwd)\n", 2));
 	if (!getcwd(cwd, 1024))
-		ft_putstr_fd("error retrieving the current working directory", fd_to_write);
+		ft_putstr_fd("error retrieving the current working directory", 2);
 	else
 		ft_putstr_fd( cwd, fd_to_write);
 	ft_putstr_fd("\n", fd_to_write);
 	free(cwd);
-}
-
-static void update_pwd(const char *dir)
-{
-
-	char * path;
-	char *dir_w_slash;
-	char * path_tmp =ft_calloc(1024, sizeof (char));
-	char * path_tmp2 =ft_calloc(1024, sizeof (char));
-
-	if (!path_tmp || !path_tmp2 || (!envp_lst && (free(path_tmp), 1)) )
-	{
-		ft_putstr_fd("an error occurred (!envp_lst || !path_tmp)\nexiting..\n", 2);
-		exit(EXIT_FAILURE);
-	}
-	path_tmp = getcwd(path_tmp, 1024);
-	if (!path_tmp)
-		return  (ft_putstr_fd("an error occurred (!path_tmp)\n", 2));
-	dir_w_slash = ft_strjoin("/", dir);
-	path = ft_strjoin(path_tmp, dir_w_slash);
-	if (!path)
-		return  (ft_putstr_fd("an error occurred (!path)\n", 2));
-	if (chdir(path) != 0)
-	{
-		perror("chdir() error");
-		exit(EXIT_FAILURE);
-	}
-	path_tmp2 = getcwd(path_tmp2, 1024);
-	char *env_var_oldpwd = ft_strjoin_secure("OLDPWD=", path_tmp);
-	char *env_var_newpwd = ft_strjoin_secure("PWD=", path_tmp2);
-	insert_or_update_env_var(create_keyval_from_env_var(env_var_oldpwd));
-	insert_or_update_env_var(create_keyval_from_env_var(env_var_newpwd));
-	free(env_var_oldpwd);
-	return (free(path),	free(dir_w_slash),	free(path_tmp), free(path_tmp2));
 }
 
 void cd(struct s_cmd *cmd)
@@ -75,8 +41,12 @@ void cd(struct s_cmd *cmd)
 	}
 	if (st.st_mode & S_IRUSR)
 	{
-		update_pwd((const char *) dir);
-		update_exit_code(0);
+		if (chdir(dir) != 0)
+		{
+			perror("chdir() error");
+			update_exit_code(0);
+		}
+		update_pwd();
 		return ;
 	}
 	else
@@ -85,4 +55,25 @@ void cd(struct s_cmd *cmd)
 		update_exit_code(errno);
 		return ;
 	}
+}
+
+static void update_pwd()
+{
+	char * path =ft_calloc(1024, sizeof (char));
+	if (!path)
+	{
+		ft_putstr_fd("an error occurred (!envp_lst || !path)\nexiting..\n", 2);
+		exit(EXIT_FAILURE);
+	}
+	path = getcwd(path, 1024);
+	if (!path)
+		return  (perror("getcwd"));
+	char *env_var_oldpwd = ft_strjoin_secure("OLDPWD=", get_env_var_value("PWD"));
+	char *env_var_newpwd = ft_strjoin_secure("PWD=", path);
+	insert_or_update_env_var(create_keyval_from_env_var(env_var_oldpwd));
+	insert_or_update_env_var(create_keyval_from_env_var(env_var_newpwd));
+	free(env_var_oldpwd);
+	free(env_var_newpwd);
+	return (free(path));
+
 }
