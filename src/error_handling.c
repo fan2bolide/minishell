4ae926d44722e8609b5e99d8e5d9ead77976b8e4
basil <basiljeannot@icloud.com>
2023-04-06 04:28:49 +0200

    fixed leaks in error handling

--- src/error_handling.c
+++ src/error_handling.c
@@ -15,29 +15,32 @@
 void	print_error(enum e_error_context context, char *token)
 {
 	char *res;
+	char *tmp;
 
 	res = "Turboshell: ";
 	if (context == parsing_error)
 	{
-		res = ft_strjoin(res, "parsing error near token: ");
-		res = ft_strjoin(res, token);
+		tmp = ft_strjoin(res, "parsing error near token: ");
+		res = ft_strjoin(tmp, token);
+		free(tmp);
 		return(ft_putstr_fd(res, STDERR_FILENO), free(res), (void)0);
 	}
-	res = ft_strjoin(res, token);
+	tmp = ft_strjoin(res, token);
 	if (context == no_file)
-		res = ft_strjoin(res, ": no such file or directory\n");
+		res = ft_strjoin(tmp, ": no such file or directory\n");
 	if (context == unknown_command)
-		res = ft_strjoin(res, ": command not found\n");
+		res = ft_strjoin(tmp, ": command not found\n");
 	if (context == perm_denied)
-		res = ft_strjoin(res, ": Permission denied\n");
+		res = ft_strjoin(tmp, ": Permission denied\n");
 	if (context == error_occured)
-		res = ft_strjoin(res, ": An error occurred\n");
+		res = ft_strjoin(tmp, ": An error occurred\n");
 	if (context == alloc_error)
-		res = ft_strjoin(res, ": allocation error\n");
+		res = ft_strjoin(tmp, ": allocation error\n");
 	if (context == cwd_error)
-		res = ft_strjoin(res, "error retrieving the current working directory\n");
+		res = ft_strjoin(tmp, "error retrieving the current working directory\n");
 	if (context == heredoc_pipe_error)
-		res = ft_strjoin(res, "error while writing in heredoc's pipe\n");
+		res = ft_strjoin(tmp, "error while writing in heredoc's pipe\n");
+	free(tmp);
 	ft_putstr_fd(res, STDERR_FILENO);
 	free(res);
 }
