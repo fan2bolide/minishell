13b663512e6b5c49aa0d3d79921ae995e11f0ed6
aLeuleu <97832618+aLeuleu@users.noreply.github.com>
2023-04-14 17:03:51 +0200

    compile but does not work properly

--- src/execute_cmd.c
+++ src/execute_cmd.c
@@ -19,21 +19,23 @@ static void	dup2_fds(int to_read, int to_write);
 //'to_read' linked to child 'stdin' and child 'stdout' linked to 'to_write'
 void	execute_cmd(t_cmd_list **cmd_list_ptr, int fds[2])
 {
-	t_cmd	cmd;
-	char	**envp;
+	t_cmd		cmd;
+	char		**envp;
+	const int	to_read = 0;
+	const int	to_write = 1;
 
-	if (to_read < 0 || to_write < 0)
-		clear_and_exit_with_exit_code(cmd);
-	cmd = *(*cmd)->content;
+	if (fds[to_read] < 0 || fds[to_write] < 0)
+		clear_and_exit_with_exit_code(cmd_list_ptr);
+	cmd = *(*cmd_list_ptr)->content;
 	if (cmd.argv && is_builtin(cmd.argv[0]) >= 0)
 	{
-		exec_builtin(cmd, to_write);
-		clear_and_exit_with_exit_code(cmd);
+		exec_builtin(cmd_list_ptr, fds[to_write]);
+		clear_and_exit_with_exit_code(cmd_list_ptr);
 	}
 	if ((cmd.argv && cmd.argv[0] && !*cmd.argv[0]) || \
 		(cmd.path && ft_strequ(cmd.path, "heredoc")))
 		exit(EXIT_SUCCESS);
-	dup2_fds(to_read, to_write);
+	dup2_fds(fds[to_read], fds[to_write]);
 	envp = (char **)ft_keyval_lst_to_str_arr(g_envp_lst);
 	ft_lstclear((t_list **)&g_envp_lst, &destroy_keyval);
 	if (cmd.path)
@@ -41,7 +43,7 @@ void	execute_cmd(t_cmd_list **cmd_list_ptr, int fds[2])
 	if (envp)
 		ft_free_arr((void *)envp, free);
 	free(envp);
-	ft_lstclear((t_list **)cmd, (void (*)(void *)) & destroy_cmd);
+	ft_lstclear((t_list **)cmd_list_ptr, (void (*)(void *)) & destroy_cmd);
 	exit(EXIT_FAILURE);
 }
 
