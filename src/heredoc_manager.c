6c5e928af725f2db4ea749ec34d3b25e282a5039
basil <basiljeannot@icloud.com>
2023-04-14 16:57:07 +0200

    fixed ctrl+D issue + remade append_new_line_if_not_delim function so it passes the norm + secured expands in it

--- src/heredoc_manager.c
+++ src/heredoc_manager.c
@@ -15,8 +15,7 @@
 
 static void	here_doc_routine(int fd_to_write, char *delimiter);
 //return 0 if new_line is not delim, return -1 if new_line is delimiter
-static int	append_new_line_if_not_delim(int fd, char **str_to_append,
-				char *delim);
+static int append_new_line_if_not_delim(char **str_to_append, char *delim);
 static int	append_str(char **str_to_append, char *next_line);
 static char	*remove_quotes(const char *raw);
 static int	is_delimiter(char *delim, const char *next_line);
@@ -36,8 +35,8 @@ static void	here_doc_routine(int fd_to_write, char *delimiter)
 
 	heredoc_buf = NULL;
 	printf("heredoc> ");
-	while (append_new_line_if_not_delim(STDIN_FILENO, &heredoc_buf,
-			delimiter) == 0)
+	while (append_new_line_if_not_delim(&heredoc_buf,
+										delimiter) == 0)
 		printf("heredoc> ");
 	if (heredoc_buf)
 	{
@@ -49,36 +48,30 @@ static void	here_doc_routine(int fd_to_write, char *delimiter)
 	free(heredoc_buf);
 }
 
-static int	append_new_line_if_not_delim(int fd, char **str_to_append,
-		char *delim)
+static int append_new_line_if_not_delim(char **str_to_append, char *delim)
 {
 	char	*next_line;
+	char	*tmp;
 	int		expand_optn;
 
-	expand_optn = 0;
 	expand_optn = (ft_strchr(delim, '\"') == NULL && \
 	ft_strchr(delim, '\'') == NULL);
-	next_line = get_next_line(fd);
+	next_line = readline("");
 	if (!next_line)
-		return (printf("\n"), -1);
+		return (append_str(str_to_append, "\n"), -1);
 	if (is_delimiter(delim, next_line))
-	{
-		free(next_line);
-		return (-1);
-	}
+		return (free(next_line), append_str(str_to_append, "\n"), -1);
 	if (expand_optn)
 	{
-		if (next_line[0])
-			next_line[ft_strlen(next_line) - 1] = 0;
-		next_line = expand_content(next_line);
-		if (next_line)
-			append_str(str_to_append, next_line);
-		append_str(str_to_append, "\n");
+		tmp = expand_content(next_line);
+		if (!tmp)
+			return (free(next_line), -1);
+		next_line = tmp;
 	}
-	else
-		append_str(str_to_append, next_line);
-	free(next_line);
-	return (0);
+	if (*str_to_append)
+		append_str(str_to_append, "\n");
+	append_str(str_to_append, next_line);
+	return (free(next_line), 0);
 }
 
 static int	is_delimiter(char *delim, const char *next_line)
@@ -88,13 +81,9 @@ static int	is_delimiter(char *delim, const char *next_line)
 	char 	*delim_without_quotes;
 
 	tmp = NULL;
-	if (!next_line)
-		return (0);
 	tmp = ft_strdup((char *)next_line);
 	if (!tmp && printf("An error occurred \n"))
 		return (0);
-	if (tmp[0])
-		tmp[ft_strlen(tmp) - 1] = 0;
 	delim_without_quotes =	remove_quotes(delim);
 	if (!delim_without_quotes)
 	{
