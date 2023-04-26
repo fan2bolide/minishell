25c79ae83e2380c52569af22413e23d173f8f8d2
basil <basiljeannot@icloud.com>
2023-04-26 18:16:54 +0200

    fixed cd printing error and updating exit code if there is more than one argument

--- src/builtin_cd.c
+++ src/builtin_cd.c
@@ -19,7 +19,7 @@ void	cd(struct s_cmd *cmd)
 	int const	success = 0;
 
 	if (cmd->argv[2])
-		return (update_exit_code(0));
+		return (update_exit_code(1), print_error(too_many_args, "cd"));
 	dir = cmd->argv[1];
 	if (!dir)
 		return ;
