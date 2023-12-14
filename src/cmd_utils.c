9e8aa53c7f1dc00dfe2f9b2c21b96a38e284050b
basil <basiljeannot@icloud.com>
2023-12-14 17:04:30 +0100

    last commit before a long time

---
+++ src/cmd_utils.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cmd_utils.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alevra <alevra@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/01 14:30:13 by alevra            #+#    #+#             */
+/*   Updated: 2023/03/01 23:33:32 by alevra           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "execute_cmd_line.h"
+
+t_cmd	*create_new_cmd(void)
+{
+	t_cmd	*res;
+
+	res = ft_calloc(sizeof(t_cmd), 1);
+	if (!res)
+		return (NULL);
+	res->argv = NULL;
+	res->path = NULL;
+	res->redirect_in = NULL;
+	res->redirect_out = NULL;
+	res->redirect_out_mode = 0;
+	res->heredoc_mode = 0;
+	res->heredoc_delim = NULL;
+	res->error = false;
+	return (res);
+}
+
+void	destroy_cmd(t_cmd *cmd)
+{
+	free_cmd(cmd);
+	free(cmd);
+}
+
+void	free_cmd_lst(t_cmd_list **cmd_list)
+{
+	ft_lstclear((t_list **)cmd_list, (void *)(free_cmd));
+}
+
+void	free_cmd(t_cmd *cmd)
+{
+	free(cmd->path);
+	if (cmd->argv)
+		ft_free_arr((void **)cmd->argv, free);
+	free(cmd->argv);
+	free(cmd->redirect_out);
+	free(cmd->heredoc_delim);
+	free(cmd->redirect_in);
+}
---
+++ src/expand_content.c
@@ -0,0 +1,119 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expand_content.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bajeanno <bajeanno@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/19 03:08:22 by bajeanno          #+#    #+#             */
+/*   Updated: 2023/04/11 07:06:19 by bajeanno         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "expand_content.h"
+
+static char	*get_clean_content(char *raw_content)
+{
+	char	*clean_content;
+	size_t	i;
+
+	i = 0;
+	if (*raw_content == '\'' || *raw_content == '\"')
+		i += end_of_quote(raw_content) + 1;
+	else
+		while (raw_content[i] && !ft_strchr("\'\"", raw_content[i]))
+			i++;
+	clean_content = ft_strnew(i);
+	if (!clean_content)
+		return (NULL);
+	return (ft_strncpy(raw_content, clean_content, (int)i));
+}
+
+static t_expansion	*evaluate_token(char *raw_content)
+{
+	t_expansion	*token;
+
+	token = ft_calloc(1, sizeof(t_expansion));
+	if (!token)
+		return (NULL);
+	if (*raw_content == '\'')
+		return (token->type = quote, \
+				token->content = get_clean_content(raw_content), token);
+	if (*raw_content == '\"')
+		return (token->type = double_quote, \
+				token->content = get_clean_content(raw_content), token);
+	return (token->type = word, \
+			token->content = get_clean_content(raw_content), token);
+}
+
+static size_t	get_next_token(char *token_content)
+{
+	size_t	i;
+
+	i = 0;
+	if (*token_content == '\'')
+	{
+		i++;
+		while (token_content[i] && token_content[i] != '\'')
+			i++;
+		return (i + 1);
+	}
+	if (*token_content == '\"')
+	{
+		i++;
+		while (token_content[i] && token_content[i] != '\"')
+			i++;
+		return (i + 1);
+	}
+	while (token_content[i] && !ft_strchr("\'\"", token_content[i]))
+		i++;
+	return (i);
+}
+
+static t_list	*get_expand_token_list(char *raw_content)
+{
+	t_list	*list;
+	t_list	*curr;
+	size_t	i;
+
+	i = 0;
+	if (!raw_content || !*raw_content)
+		return (NULL);
+	list = ft_lstnew(evaluate_token(raw_content));
+	curr = list;
+	if (!curr)
+		return (ft_lstclear(&list, destroy_expand_token), NULL);
+	curr->next = (NULL);
+	i = get_next_token(raw_content);
+	while (raw_content[i])
+	{
+		curr->next = ft_lstnew(evaluate_token(raw_content + i));
+		curr = curr->next;
+		if (!curr)
+			return (ft_lstclear(&list, destroy_expand_token), NULL);
+		i += get_next_token(raw_content + i);
+	}
+	return (list);
+}
+
+char	*expand_content(char *raw_content)
+{
+	t_list	*list;
+	t_list	*curr;
+
+	if (!raw_content || !*raw_content)
+		return (raw_content);
+	list = get_expand_token_list(raw_content);
+	remove_quotes(list);
+	curr = list;
+	while (curr)
+	{
+		if (!replace_with_value(curr->content))
+			return (ft_lstclear(&list, destroy_expand_token), NULL);
+		curr = curr->next;
+	}
+	free(raw_content);
+	raw_content = join_contents(list);
+	ft_lstclear(&list, destroy_expand_token);
+	return (raw_content);
+}
